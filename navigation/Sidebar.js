0e1e7675a11c42da46589b2bc003697dc4c5e09c
ram-prasad23 <ramsteyn17@gmail.com>
2023-08-31 20:32:30 +0530

    Onboarding screen done

--- navigation/Sidebar.js
+++ navigation/Sidebar.js
@@ -4,42 +4,58 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import LoginScreen from "../screens/LoginScreen";
 import SignUpScreen from "../screens/SignUpScreen";
 import DrawerScreen from "../Drawer/DrawerScreen";
+import OnboardingScreen from "../screens/OnboardingScreen";
 import { getAccessToken } from "../utils/get-access-token";
 import { removeAccessToken } from "../utils/remove-access-token";
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigation() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [isOnboarding, setIsOnboarding] = useState(false)
   //removeAccessToken();
   getAccessToken(setIsLoggedIn)
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        {isLoggedIn ?
+      <Stack.Navigator initialRouteName="OnBoard">
+        {!isOnboarding ?
           <>
             <Stack.Screen
-              name="Dashboard"
+              name="OnBoard"
               options={{ headerShown: false }}
-              component={DrawerScreen} />
-          </>
-          :
-          <>
+              component={OnboardingScreen}
+            />
             <Stack.Screen
               name="Login"
               options={{ headerShown: false }}
               component={LoginScreen}
             />
-            <Stack.Screen
-              name="SignUp"
-              options={{ headerShown: false }}
-              component={SignUpScreen}
-            />
-            <Stack.Screen
-              name="Dashboard"
-              options={{ headerShown: false }}
-              component={DrawerScreen}
-            />
           </>
+          :
+          isLoggedIn ?
+            <>
+              <Stack.Screen
+                name="Dashboard"
+                options={{ headerShown: false }}
+                component={DrawerScreen} />
+            </>
+            :
+            <>
+              <Stack.Screen
+                name="Login"
+                options={{ headerShown: false }}
+                component={LoginScreen}
+              />
+              <Stack.Screen
+                name="SignUp"
+                options={{ headerShown: false }}
+                component={SignUpScreen}
+              />
+              <Stack.Screen
+                name="Dashboard"
+                options={{ headerShown: false }}
+                component={DrawerScreen}
+              />
+            </>
         }
       </Stack.Navigator>
     </NavigationContainer>
